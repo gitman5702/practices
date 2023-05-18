61dcd2b8f2e48c2f05151aa4822ca5a97854aa87
gitman5702 <peoples.nick88@gmail.com>
2023-05-17 23:32:25 -0400

    practices

---
+++ practic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "What is your name?"
+read na
+
+echo "How old are you?"
+read ol
+
+echo "Where are you from?"
+read fr
+
+echo "Hello ${na}, from ${ol}. How's ${fr} in the summer."
